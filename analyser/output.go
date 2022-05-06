e3ec9e059735c35daff5e537047c90910fea14e2
luckelucky <officialluckelucky@gmail.com>
2022-05-06 14:11:59 +0100

    new output

--- analyser/output.go
+++ analyser/output.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 
 	"github.com/LuckeLucky/demo-analyser-csgo/utils"
-	"github.com/fatih/color"
+	"github.com/pterm/pterm"
 )
 
 func (analyser *Analyser) printHalfs() {
@@ -25,11 +25,7 @@ func (analyser *Analyser) printHalfs() {
 }
 
 func (analyser *Analyser) printMap() {
-	if utils.IsWindows() {
-		fmt.Fprintf(color.Output, "Map:%s\n", color.YellowString(analyser.mapName))
-	} else {
-		fmt.Printf("Map:%s\n", color.YellowString(analyser.mapName))
-	}
+	pterm.Println("Map: " + pterm.Yellow(analyser.mapName))
 }
 
 func (analyser *Analyser) printFinish() {
@@ -49,23 +45,15 @@ func printScoresHalf(half *Half, nHalf int, isCTOnLeft bool) {
 	tScore := utils.PadSpaceEnd(strconv.Itoa(half.halfTScore), 6)
 
 	halfPrint := utils.PadSpaceEnd(strconv.Itoa(nHalf), 6)
-	format := []string{color.RedString(tScore), color.BlueString(ctScore)}
+	format := []string{pterm.Red(tScore), pterm.Blue(ctScore)}
 	if isCTOnLeft {
 		format[0], format[1] = format[1], format[0]
 	}
 	fmt.Printf("%s|", halfPrint)
-	if utils.IsWindows() {
-		fmt.Fprintf(color.Output, "%s|%s\n", format[0], format[1])
-	} else {
-		fmt.Printf("%s|%s\n", format[0], format[1])
-	}
+	pterm.Println(format[0] + "|" + format[1])
 }
 func (analyser *Analyser) printScore() {
 	ctName := analyser.parser.GameState().TeamCounterTerrorists().ClanName()
 	tName := analyser.parser.GameState().TeamTerrorists().ClanName()
-	if utils.IsWindows() {
-		fmt.Fprintf(color.Output, "%s vs %s  %d : %d\n", color.BlueString(ctName), color.RedString(tName), analyser.ctScore, analyser.tScore)
-	} else {
-		fmt.Printf("%s vs %s  %d : %d\n", color.BlueString(ctName), color.RedString(tName), analyser.ctScore, analyser.tScore)
-	}
+	pterm.Printf("%s vs %s  %d : %d\n", pterm.Blue(ctName), pterm.Red(tName), analyser.ctScore, analyser.tScore)
 }
--- go.mod
+++ go.mod
@@ -7,9 +7,9 @@ require (
 	github.com/gogo/protobuf v1.3.2
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/markus-wa/demoinfocs-golang/v2 v2.10.1
+	github.com/pterm/pterm v0.12.41 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
